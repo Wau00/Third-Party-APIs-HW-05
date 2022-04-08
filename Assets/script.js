995082b5c96fa1778550b446fbabd57ca156c041
walterunderwood <walonsounderwood@gmail.com>
2022-04-07 22:24:18 -0700

    Setting conditional functions for relation between the row color and the current hour

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,23 @@
+var currentHour = moment().hours();
+console.log(currentHour);
 
 
 // Add  today's date using Moment.js //
 $("#currentDay").text(moment().format("dddd, MMMM Do "));
 
+// Relationship of the row color with respect to current time //
+function colorChange(){ 
+    var guessHour = parseInt($(this).attr('id'))
+    console.log(guessHour);
+
+    if (currentHour === guessHour){
+    $("col-sm-10").addClass("present");
+}
+else if (currentHour < guessHour){
+    $("col-sm-10").addClass("future");
+}
+    else {
+        $("col-sm-10").addClass("past");
+    }
+
+}
