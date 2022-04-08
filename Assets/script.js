77f46facf12a55cf6cc3b4ce8b6577c4e374b878
walterunderwood <walonsounderwood@gmail.com>
2022-04-08 01:06:10 -0700

    Debuggin the LocalStorage, setting childrens and parents

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,44 @@
+
+// Setting Global Variables // 
+
 var currentHour = moment().hours();
 var timer = $('.time-block');
 var saveBtn = $('.saveBtn');
 
+// Setting each id (children) to a parent (textarea) // 
+$('#9').children("textarea").val(localStorage.getItem('9'));
+
+$('#10').children("textarea").val(localStorage.getItem('10'));
+
+$('#11').children("textarea").val(localStorage.getItem('11'));
+
+$('#12').children("textarea").val(localStorage.getItem('12'));
+
+$('#13').children("textarea").val(localStorage.getItem('13'))
+
+$('#14').children("textarea").val(localStorage.getItem('14'));
+
+$('#15').children("textarea").val(localStorage.getItem('15'));
+
+$('#16').children("textarea").val(localStorage.getItem('16'));
+
+$('#17').children("textarea").val(localStorage.getItem('17'));
+
+$('#18').children("textarea").val(localStorage.getItem('18'));
+
+
 // Add  today's date using Moment.js //
 $("#currentDay").text(moment().format("dddd, MMMM Do "));
-
 
 // Local Storage //
 
 saveBtn.on('click', function() {
-var explanation = $(this).siblings(".explanation").val();
+var explanation = $("textarea").val().trim();
 var schedule = $(this).parent().attr("id");
-localStorage.setItem(explanation, schedule);
+localStorage.setItem(schedule, explanation);
+});
 
-})
-
-
-
-
-// Relationship of the row color with respect to current time //
+// Conditions of row color with respect to current time //
 
 function colorChange() { 
     var currentHour = moment().hours(); 
