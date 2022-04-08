ce729e763a378e6b5e2079e85bfa5d0063e5660e
walterunderwood <walonsounderwood@gmail.com>
2022-04-07 23:49:10 -0700

    Adding the property of Local Storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,31 @@
 var currentHour = moment().hours();
 var timer = $('.time-block');
-
+var saveBtn = $('.saveBtn');
 
 // Add  today's date using Moment.js //
 $("#currentDay").text(moment().format("dddd, MMMM Do "));
 
+
+// Local Storage //
+
+saveBtn.on('click', function() {
+var explanation = $(this).siblings(".explanation").val();
+var schedule = $(this).parent().attr("id");
+localStorage.setItem(explanation, schedule);
+
+})
+
+
+
+
 // Relationship of the row color with respect to current time //
 
 function colorChange() { 
-    var currentHour = moment().hours();
+    var currentHour = moment().hours(); 
     
-
-
 timer.each(function() { 
     var guessHour = parseInt($(this).attr('id'))
+
     
     if (currentHour === guessHour){
     $(this).children(".col-sm-10").addClass("present")
